eec50003a716e847750ef36486a3aca7414afa32
jing-an-chen <chen0969@algonquinlive.com>
2023-10-17 15:26:45 -0400

    more function

--- function/functions.js
+++ function/functions.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
-    $("#btn-home").click(function(){
-        
-    })
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var w_scroll = $(window).scrollTop();
+        var nav_p = $("#menu-bar").offset().top;
+
+        if (w_scroll >= nav_p) {
+            $("#menu-bar").addClass("sticktop");
+        } else {
+            $("#menu-bar").removeClass("sticktop");
+        };
+    });
 });
+
